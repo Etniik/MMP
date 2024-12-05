bad9db51ae536a2caa53a9a2382f02563a086a95
Etniik <etnik332@gmail.com>
2024-12-05 17:43:22 +0100

    a

---
+++ include/db.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'mmp';
+try {
+    $pdo = new PDO('mysql:host=$host;dbname=$dbname', $user, $pass );
+} catch (PDOException $e) {
+    echo "Error" . $e->getMessage();
+}
+?>
